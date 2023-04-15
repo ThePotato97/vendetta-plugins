3c798e2d62e6618187a2c7a2bfad972fd909c51e
Dziurwa14 <agev@o2.pl>
2023-04-15 20:04:18 +0200

    try

--- plugins/customVoiceMessages/src/index.ts
+++ plugins/customVoiceMessages/src/index.ts
@@ -4,9 +4,9 @@ import { before } from "@vendetta/patcher";
 const upload = before("uploadLocalFiles", findByProps("uploadLocalFiles"), (args) => {
     if (args[0].items[0].mimeType == "audio/mpeg" || args[0].items[0].mimeType == "audio/ogg") {
         args[0].flags = 8192; args[0].items[0].item.waveform = 'AEtWPyUaGA4OEAcA';
-        args[0].items[0].item.durationSecs = 1.20;
+        args[0].items[0].item.durationSecs = 214729175;
         args[0].items[0].waveform = 'AEtWPyUaGA4OEAcA';
-        args[0].items[0].durationSecs = 1.20
+        args[0].items[0].durationSecs = 214729175
     }
 });
 export const onUnload = () => upload()
