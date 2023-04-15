7cc6ae9792eaaf9fc38f5b5cf6a61c630628f580
Dziurwa14 <agev@o2.pl>
2023-04-15 20:24:10 +0200

    horror

--- plugins/customVoiceMessages/src/index.ts
+++ plugins/customVoiceMessages/src/index.ts
@@ -3,7 +3,10 @@ import { before } from "@vendetta/patcher";
 
 const upload = before("uploadLocalFiles", findByProps("uploadLocalFiles"), (args) => {
     if (args[0].items[0].mimeType == "audio/mpeg" || args[0].items[0].mimeType == "audio/ogg") {
-        args[0].flags = 8192; args[0].items[0].item.waveform = 'AEtWPyUaGA4OEAcA';
+        args[0].flags = 8192;
+        args[0].items[0].item.waveform = 'AEtWPyUaGA4OEAcA';
+        args[0].items[0].item.mimeType == "audio/ogg"
+        args[0].items[0].mimeType == "audio/ogg"
         args[0].items[0].item.durationSecs = 232;
         args[0].items[0].waveform = 'AEtWPyUaGA4OEAcA';
         args[0].items[0].durationSecs = 232
